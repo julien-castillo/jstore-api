0c3eb837e6cb7d5df89c05a2a761ad30a4931b56
Julien Castillo <juliencastillo77@gmail.com>
2022-04-24 10:06:59 +0200

    add migrations and fixtures

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,21 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
+
+    /**
+     * @route("/hello")
+     */
     public function hello(): Response {
-        return new Response("Hello WOrld");
+        return new Response("Hello World");
+    }
+
+    /**
+     * @route("/hello/{name}")
+     */
+    public function helloName($name): Response {
+        return new Response("Hello ".$name);
     }
 }
