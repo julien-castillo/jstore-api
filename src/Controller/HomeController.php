af9f463a25d544f3c60ac4f329aab635acf715b5
Julien Castillo <juliencastillo77@gmail.com>
2022-04-24 13:10:48 +0200

    add template index with cards

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,12 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+
+
+        return $this->render('home/index.html.twig', [
+            'articles' =>$articles,
+        ]);
     }
 }
